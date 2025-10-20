d9632f914ad77c229f54e693dd0f42d9e9588116
ian060756 <ian060756@gmail.com>
2025-10-21 00:40:01 +0800

    rework w5

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 export default function App(){
   return(
-    <div>
+    <div className="min-h-screen flex flex-col bg-base-100">
       <Header/>
-      <Outlet/>
+      <main className="flex-1 container mx-auto px-4 py-8">
+        <Outlet/>
+      </main>   
       <Footer/>
     </div>
   );
